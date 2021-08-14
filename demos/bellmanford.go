f1542645e590f53a856968a8c72d67bc6745b896
Diefesson de Sousa Silva <diefesson.so@gmail.com>
2021-08-14 16:18:39 -0300

    refactor: updated to use reverse Bellman-Ford

--- demos/bellmanford.go
+++ demos/bellmanford.go
@@ -3,12 +3,17 @@ package demos
 import (
 	"fmt"
 
+	"github.com/diefesson/ufc-ga/graph"
 	"github.com/diefesson/ufc-ga/graph/algorithms"
 )
 
 func BellmanFordDemo() {
 	cities := createDiCities()
-	distance, path := algorithms.BellmanFord(cities, 5, 1, createDistanceCalculator(createCoordinates()))
+	path := make([]int, 0)
+	distance, err := algorithms.BellmanFord(cities, 5, 1, createDistanceCalculator(createCoordinates()), graph.CollectVerticesInto(&path))
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Distance:", distance)
 	fmt.Println("Path:", path)
 }
--- graph/algorithms/bellmanford.go
+++ graph/algorithms/bellmanford.go
@@ -1,47 +1,47 @@
 package algorithms
 
 import (
+	"errors"
 	"math"
 
 	"github.com/diefesson/ufc-ga/graph"
 )
 
-func BellmanFord(g *graph.DiGraph, from, to int, dc graph.DistanceCalculator) (float64, []int) {
-	path := make([]int, 0)
-	predecessors := make([]int, g.Capacity())
-	for i := 0; i < len(predecessors); i++ {
-		predecessors[i] = -1
-	}
+func BellmanFord(g *graph.DiGraph, from, to int, dc graph.DistanceCalculator, vp graph.VertexProcessor) (float64, error) {
+	sucessors := make([]int, g.Capacity())
 	distances := make([]float64, g.Capacity())
 	for i := 0; i < len(distances); i++ {
+		sucessors[i] = -1
 		distances[i] = math.Inf(1)
 	}
-	distances[from] = 0
+	distances[to] = 0
 
 	for i := 0; i < g.VertexCount(); i++ {
-		g.ForEdges(graph.IfConnected(func(_ graph.Graph, from, to int) {
-			baseDistance := distances[from]
-			currentDistance := distances[to]
-			newDistance := baseDistance + dc(g, from, to)
-			if newDistance < currentDistance {
-				distances[to] = newDistance
-				predecessors[to] = from
+		g.ForEdges(graph.IfConnected(func(g graph.Graph, f, t int) {
+			newDistance := distances[t] + dc(g, f, t)
+			if newDistance < distances[f] {
+				distances[f] = newDistance
+				sucessors[f] = t
 			}
 		}))
 	}
 
-	if predecessors[to] == -1 {
-		panic("Theres no valid shortest path")
+	if sucessors[from] == -1 {
+		return -1, errors.New("could not find shortest path")
 	}
-	g.ForEdges(graph.IfConnected(func(g graph.Graph, from, to int) {
-		if distances[from]+dc(g, from, to) < distances[to] {
-			panic("Graph contains negative weight")
+	for t := 0; t < g.Capacity(); t++ {
+		for f := 0; f < g.Capacity(); f++ {
+			if g.IsConnected(f, t) && distances[t]+dc(g, f, t) < distances[f] {
+				return -1, errors.New("graph contains negative weight")
+			}
 		}
-	}))
-
-	for v := to; v != -1; v = predecessors[v] {
-		path = append(path, v)
 	}
-	reverse(path)
-	return distances[to], path
+
+	v := from
+	for v != to {
+		vp(g, v)
+		v = sucessors[v]
+	}
+	vp(g, v)
+	return distances[from], nil
 }
